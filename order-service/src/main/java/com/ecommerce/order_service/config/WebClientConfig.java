f86e49dca35d68519321cdc788be40c5486c177e
steeltx <realsteel.gnu@gmail.com>
2026-07-26 00:01:17 -0600

    order - ajuste para comunicación mediante PutExchange, WebClient, ProxyFactory  y WebClientAdapter

--- order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
+++ order-service/src/main/java/com/ecommerce/order_service/config/WebClientConfig.java
@@ -1,13 +1,26 @@
 package com.ecommerce.order_service.config;
 
+import com.ecommerce.order_service.service.client.InventoryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient.Builder webClientBuilder(){
-        return WebClient.builder();
+    public WebClient webClientBuilder(){
+        return WebClient.builder()
+                .baseUrl("http://localhost:8082")
+                .build();
+    }
+
+    @Bean
+    public InventoryClient inventoryClient(WebClient webClient){
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+                .builderFor(WebClientAdapter
+                .create(webClient)).build();
+        return factory.createClient(InventoryClient.class);
     }
 }
--- order-service/src/main/java/com/ecommerce/order_service/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/ecommerce/order_service/service/impl/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import com.ecommerce.order_service.model.Order;
 import com.ecommerce.order_service.model.OrderLineItems;
 import com.ecommerce.order_service.repository.OrderRepository;
 import com.ecommerce.order_service.service.OrderService;
+import com.ecommerce.order_service.service.client.InventoryClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,7 +25,8 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
-    private final WebClient.Builder webClientBuilder;
+//    private final WebClient.Builder webClientBuilder;
+    private final InventoryClient inventoryClient;
 
     @Override
     @Transactional
@@ -36,12 +38,13 @@ public class OrderServiceImpl implements OrderService {
             String sku = item.getSku();
             Integer quantity = item.getQuantity();
             try {
-                webClientBuilder.build().put()
-                        .uri("http://localhost:8082/api/v1/inventory/reduce/"+sku,
-                                uriBuilder -> uriBuilder.queryParam("quantity",quantity).build())
-                        .retrieve()
-                        .bodyToMono(String.class)
-                        .block();
+//                webClientBuilder.build().put()
+//                        .uri("http://localhost:8082/api/v1/inventory/reduce/"+sku,
+//                                uriBuilder -> uriBuilder.queryParam("quantity",quantity).build())
+//                        .retrieve()
+//                        .bodyToMono(String.class)
+//                        .block();
+                inventoryClient.reduceStock(sku, quantity);
             }catch (Exception e){
                 log.error("Error al reducir stock para el producto {}: {}", sku, e.getMessage());
                 throw new IllegalArgumentException("No se pudo procesar la orden: Stock insuficiente/Error de inventario");
