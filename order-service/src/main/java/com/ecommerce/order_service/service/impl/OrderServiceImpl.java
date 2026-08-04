c03452b13ecc9f46d22ed6eaa27b758dc3681c2f
steeltx <realsteel.gnu@gmail.com>
2026-08-04 17:46:09 -0600

    config - se agregan configuraciones en inventory, order y product para consultar datos de config server

--- order-service/src/main/java/com/ecommerce/order_service/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/ecommerce/order_service/service/impl/OrderServiceImpl.java
@@ -11,6 +11,8 @@ import com.ecommerce.order_service.service.OrderService;
 import com.ecommerce.order_service.service.client.InventoryClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -21,6 +23,7 @@ import java.util.UUID;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@RefreshScope
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
@@ -28,9 +31,16 @@ public class OrderServiceImpl implements OrderService {
 //    private final WebClient.Builder webClientBuilder;
     private final InventoryClient inventoryClient;
 
+    @Value("${order.enabled:true}")
+    private boolean ordersEnabled;
+
     @Override
     @Transactional
     public OrderResponse placeOrder(OrderRequest orderRequest) {
+        if(!ordersEnabled){
+            log.warn("Pedido rechazado, servicio deshabilitado por configuración");
+            throw new RuntimeException("El servicio de pedidos esta actualmente en mantenimiento");
+        }
         log.info("Colocando nuevo pedido");
         Order order = orderMapper.toOrder(orderRequest);
 
--- product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.ecommerce.product_service.controller;
 import com.ecommerce.product_service.dto.ProductRequestDto;
 import com.ecommerce.product_service.dto.ProductResponseDto;
 import com.ecommerce.product_service.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +16,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/product")
 @RequiredArgsConstructor
+@RefreshScope
 public class ProductController {
 
     private final ProductService productService;
+    @Value("${app.maintenance.message: Sistema operativo}")
+    private String maintenanceMessage;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -25,7 +31,8 @@ public class ProductController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponseDto> getAllProducts(){
+    public List<ProductResponseDto> getAllProducts(HttpServletResponse response){
+        response.addHeader("X-Maintenance-Message",maintenanceMessage);
         return productService.getAllsProducts();
     }
 
