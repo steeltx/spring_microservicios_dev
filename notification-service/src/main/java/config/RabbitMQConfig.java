6d040d9586fc595f4534d23ddb650a30360a2271
steeltx <77936200+steeltx@users.noreply.github.com>
2026-08-22 23:38:48 -0600

    rabbit - configuración de MessageConverter y dependencia amqp

---
+++ notification-service/src/main/java/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+}
