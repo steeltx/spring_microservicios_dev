5e75a1bcba35b22a21efafdd9346404a4c32f11f
steeltx <77936200+steeltx@users.noreply.github.com>
2026-08-24 19:34:03 -0600

    rabbit - creación de OrderPlacedEvent para inventory, notification y order

---
+++ order-service/src/main/java/com/ecommerce/order_service/event/OrderPlacedEvent.java
@@ -0,0 +1,15 @@
+package com.ecommerce.order_service.event;
+
+import java.util.List;
+
+public record OrderPlacedEvent(
+        String orderNumber,
+        String email,
+        List<OrderItemEvent> items
+) {
+    public record OrderItemEvent(
+            String sku,
+            String price,
+            Integer quantity
+    ){}
+}
